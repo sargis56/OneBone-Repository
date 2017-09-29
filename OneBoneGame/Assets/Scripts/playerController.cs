cb0f05b67d59f211f0ff15e547c7b89eaf0f38c1
aldrin312 <aldrin_fernandez312@yahoo.com>
2017-09-29 14:46:59 -0400

    comments in player controller

--- OneBoneGame/Assets/Scripts/playerController.cs
+++ OneBoneGame/Assets/Scripts/playerController.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 [System.Serializable]
 public class playerController : MonoBehaviour {
-
+	//public varriables
 	public float speed = 5.0f;
-	public float groundRadius;
+	public float groundRadius; //radius of the groundcheck
 	public float jumpForce = 700f;
+
+	public Transform groundCheck;
+	public LayerMask whatIsGround; //layers of that player can jump on (this case everything)
+
+	//ground check
 	bool grounded = false;
 
 	Rigidbody2D rBody;
 
-	public Transform groundCheck;
-	public LayerMask whatIsGround;
+
 	// Use this for initialization
 	void Start () {
 		rBody = this.GetComponent<Rigidbody2D>();
@@ -20,18 +24,16 @@ public class playerController : MonoBehaviour {
 
 	private void FixedUpdate()
 	{
-		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround); //check if groundcheck overlap with the ground
 		float horizMove = Input.GetAxis("Horizontal");
 
-		rBody.velocity = new Vector2(horizMove * speed, rBody.velocity.y);
-
-		Debug.Log(grounded);
-
+		rBody.velocity = new Vector2(horizMove * speed, rBody.velocity.y); // basic movement
+	
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if ((grounded) && Input.GetButtonDown("Jump"))
+		if ((grounded) && Input.GetButtonDown("Jump")) //checking for the input and if groundcheck is overlapping with the ground
 		{
 
 			rBody.AddForce(new Vector2(0, jumpForce));
