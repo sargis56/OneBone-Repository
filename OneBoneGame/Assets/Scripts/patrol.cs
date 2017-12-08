d1fc7ea70ff312cf8648d667ef7178473d1a0764
Sargis <sargis@live.ca>
2017-12-07 23:03:52 -0500

    Level 3, Scene loader, and moving enemies
    Added level 3, Scene loader, and moving  enemies

---
+++ OneBoneGame/Assets/Scripts/patrol.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class patrol : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public double moveLeft;
+    public double moveRight;
+    GameObject gameControllerObject;
+    public int speed = 1;
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+    }
+
+    void Update()
+    {
+        if (this.gameObject.tag == "enemy")
+        {
+            if (rb.position.x > moveRight)
+            {
+                //rb.velocity = new Vector2(speed, 0);
+                rb.velocity = new Vector2(speed * -1, 0);
+            }
+            if (rb.position.x < moveLeft)
+            {
+                rb.velocity = new Vector2(speed, 0);
+                //rb.velocity = new Vector2(speed * -1, 0);
+            }
+        }
+        else
+        {
+            Debug.Log("Cannot find Game Object");
+        }
+    }
+}
